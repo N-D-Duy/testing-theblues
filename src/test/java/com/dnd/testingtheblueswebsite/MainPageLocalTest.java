e69bcee0bd522b1d09b77595919ac3323472c64b
N-D-Duy <Ct060311@actvn.edu.vn>
2025-05-30 08:16:41 +0700

    fix: use selenium instead of selenide, fix performance debug err, update selenium version

--- src/test/java/com/dnd/testingtheblueswebsite/MainPageLocalTest.java
+++ src/test/java/com/dnd/testingtheblueswebsite/MainPageLocalTest.java
@@ -1,4 +1,17 @@
 package com.dnd.testingtheblueswebsite;
 
-public class MainPageLocalTest {
+import org.testng.annotations.Test;
+
+import java.util.Objects;
+
+@Test(groups = "local")
+public class MainPageLocalTest extends SeleniumTest {
+
+    @Test
+    public void testMainPageTitle() {
+        driver.get("https://jetbrains.com/");
+        String expectedTitle = "JetBrains: Essential tools for software developers and teams";
+        String actualTitle = driver.getTitle();
+        assert Objects.equals(actualTitle, expectedTitle) : "Expected title: " + expectedTitle + ", but got: " + actualTitle;
+    }
 }
--- src/test/java/com/dnd/testingtheblueswebsite/MainPageTest.java
+++ src/test/java/com/dnd/testingtheblueswebsite/MainPageTest.java
@@ -1,60 +1,16 @@
 package com.dnd.testingtheblueswebsite;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 
-import java.time.Duration;
+import java.util.Objects;
 
-import static org.testng.Assert.*;
-
-import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
-
-public class MainPageTest {
-    MainPage mainPage = new MainPage();
-
-    @BeforeClass
-    public static void setUpAll() {
-        Configuration.browserSize = "1280x800";
-        SelenideLogger.addListener("allure", new AllureSelenide());
-    }
-
-    @BeforeMethod
-    public void setUp() {
-        open("https://www.jetbrains.com/");
-        if ($("button.ch2-btn.ch2-allow-all-btn.ch2-btn-primary").shouldBe(visible, Duration.ofSeconds(5)).exists()) {
-            $("button.ch2-btn.ch2-allow-all-btn.ch2-btn-primary").click();
-        }
-    }
+public class MainPageTest extends SeleniumTest{
 
     @Test
-    public void search() {
-        mainPage.searchButton.click();
-
-        $("[data-test='search-input']").sendKeys("Selenium");
-        $("button[data-test='full-search-button']").click();
-
-        $("input[data-test='search-input']").shouldHave(attribute("value", "Selenium"));
-    }
-
-    @Test
-    public void toolsMenu() {
-        mainPage.toolsMenu.click();
-
-        $("div[data-test='main-submenu']").shouldBe(visible);
-    }
-
-    @Test
-    public void navigationToAllTools() {
-        mainPage.seeDeveloperToolsButton.click();
-        mainPage.findYourToolsButton.click();
-
-        $("#products-page").shouldBe(visible);
-
-        assertEquals(Selenide.title(), "All Developer Tools and Products by JetBrains");
+    public void testMainPageTitle() {
+        driver.get("https://jetbrains.com/");
+        String expectedTitle = "JetBrains: Essential tools for software developers and teams";
+        String actualTitle = driver.getTitle();
+        assert Objects.equals(actualTitle, expectedTitle) : "Expected title: " + expectedTitle + ", but got: " + actualTitle;
     }
 }
